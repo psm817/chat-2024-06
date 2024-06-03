49a264de3aecb03b061ee8143a008c7f9795a75f
psm817 <5004jse@gmail.com>
2024-06-03 16:15:39 +0900

    post romm make, room list

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
@@ -1,23 +1,44 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.controller;
 
+import com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatRoom;
+import com.ll.chat_2024_06_03.domain.chat.chatRoom.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/chat/room")
 public class chatRoomController {
+    private final ChatRoomService chatRoomService;
+
     @GetMapping("/{roomId}")
     @ResponseBody
     public String showRoom(
             @PathVariable("roomId") final long roomId,
-            @RequestParam(value="writerName", defaultValue = "NoName") final String writerName
+            @RequestParam(value = "writerName", defaultValue = "NoName") final String writerName
     ) {
-
-        return "%d번 채팅방 입니다. 작성자 : %s".formatted(roomId, writerName);
+        return "%d번 채팅방 입니다. writer : %s".formatted(roomId, writerName);
     }
 
     @GetMapping("/make")
     public String showMake() {
         return "domain/chat/chatRoom/make";
+    }
+
+    @PostMapping("/make")
+    public String make(
+            @RequestParam(value = "name") final String name
+    ) {
+        chatRoomService.make(name);
+        return "redirect:/chat/room/make?message=Chat Room Created";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<ChatRoom> showList() {
+        return chatRoomService.findAll();
     }
 }
