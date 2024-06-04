df576952aefe0bcd57637af27731c9c1f8f9a1f3
psm817 <5004jse@gmail.com>
2024-06-04 14:19:43 +0900

    use JPA

--- src/main/java/com/ll/chat_2024_06_03/Chat20240603Application.java
+++ src/main/java/com/ll/chat_2024_06_03/Chat20240603Application.java
@@ -2,8 +2,10 @@ package com.ll.chat_2024_06_03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing 			// @CreateDate, @ModifiedDate를 사용하기 위해 필요
 public class Chat20240603Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,12 +1,34 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     @Getter
-    @Setter
-    private long id;
+    private Long id;
+
+    @CreatedDate
+    @Getter
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Getter
+    private LocalDateTime modifyDate;
 
     @Getter
     private String name;
